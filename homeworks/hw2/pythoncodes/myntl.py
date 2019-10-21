2d1ec309df0a6567719cc377fa535a66fa530dac
herentug <herentug@sabanciuniv.edu>
2019-10-21 23:44:57 +0300

    homework 2 implemented

---
+++ homeworks/hw2/pythoncodes/myntl.py
@@ -0,0 +1,58 @@
+import math
+import random
+import pyprimes
+import warnings
+
+def phi(n):
+    amount = 0
+    for k in range(1, n + 1):
+        if math.gcd(n, k) == 1:
+            amount += 1
+    return amount
+
+def gcd(a, b):
+    """Calculate the Greatest Common Divisor of a and b.
+
+    Unless b==0, the result will have the same sign as b (so that when
+    b is divided by it, the result comes out positive).
+    """
+    while b:
+        a, b = b, a%b
+    return a
+
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
+
+def modinv(a, m):
+    gcd, x, y = egcd(a, m)
+    if gcd != 1:
+        return None  # modular inverse does not exist
+    else:
+        return x % m
+      
+small_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+                113]
+
+def check_small_primes(p):
+    for i in small_primes:
+        if p%i==0:
+            return -1
+    return 1
+    
+def random_prime(bound):
+    warnings.simplefilter('ignore')
+    chck = False
+    while chck == False:
+        p = random.randrange(1, bound)
+        if check_small_primes(p) == 1:
+            chck = pyprimes.isprime(p)
+    warnings.simplefilter('default')    
+    return p   
+
